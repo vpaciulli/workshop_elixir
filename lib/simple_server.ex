6c101f68d4e9ee1606b85a8ec7ab2a8e2ba9694a
Lucca <julienlucca@gmail.com>
2016-01-30 05:06:02 -0200

    impl

--- lib/simple_server.ex
+++ lib/simple_server.ex
@@ -1,22 +1,25 @@
 defmodule SimpleServer do
-
-  @queue = []
-
   def start do
-    pid = spawn(__MODULE__, :loop)
+    words_pid = Words.start_link
+    right_answer = "123456" |> Base64.encode64
+    pid = spawn(__MODULE__, :loop, [word_pid, right_answer])
     :yes = :global.register_name("simple_server", pid) # register it globally
     pid
   end
 
-  def loop do
+  def loop(word_pid, right_answer) do
     receive do
-      {:check, worker, "123"} -> send(worker, {:ok, "123"})
-      {:new_key, worker} -> send(worker, pop)
+      {:request, worker_pid} ->
+        test_this_word = Words.pop(word_pid)
+        send(worker_pid, test_this_word, right_answer)
+        loop(word_pid, right_answer)
+
+      {true, right_word} ->
+        IO.puts("======== Found the right word: #{right_word}")
+        loop(word_pid, right_answer)
+
+      {false, _} ->
+        loop(word_pid, right_answer)
     end
   end
-
-  def pop([]) do
-    
-  end
-
 end
